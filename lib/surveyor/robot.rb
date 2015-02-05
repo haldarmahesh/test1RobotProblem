2b49d64ae0a14212ece8ed0bb7cbbecefbb9543e
haldarmahesh <maheshkumar@Maheshs-MacBook-Pro.local>
2015-02-05 12:10:43 +0530

    Robot spec change orientation successfull

--- lib/surveyor/robot.rb
+++ lib/surveyor/robot.rb
@@ -1,8 +1,10 @@
 #this sets and returns attributes of a robot like orientation, moves and curent coordinate
 class Robot
+ 
   def initialize
     @orientation = ""
     @position = [0,0]
+    @direction_array = ["NORTH", "EAST", "SOUTH", "WEST"]
   end
 
   def set_position(x, y)
@@ -10,7 +12,33 @@ class Robot
     @position[1] = y
     @position
   end
-  def change_orientation(orientation)
+  def set_orientation(orientation)
     @orientation = orientation
   end
+  def change_orientation(direction)
+    
+    # hash = Hash[@direction_array.map.with_index.to_a]  
+    current_index = @direction_array.index(@orientation)
+
+    case direction
+    
+    when "LEFT"
+      current_index = (current_index - 1 ) % 4  
+    when "RIGHT"
+      current_index = (current_index + 1 ) % 4
+    end
+    @orientation = @direction_array[current_index]
+    return @orientation
+  end
+  def move
+    case @orientation
+      when "LEFT"
+        @position[0] -= @position[0]
+        @position
+      when "RIGHT"
+        @position[1] += @position[1]
+    end  
+    @position
+  end
+
 end
--- spec/surveyor/robot_spec.rb
+++ spec/surveyor/robot_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 describe "Robot" do
   let(:robot1) { Robot.new }
   it "sets the direction of robot" do
-    expect(robot1.change_orientation("NORTH")).to eq("NORTH")
+    expect(robot1.set_orientation("NORTH")).to eq("NORTH")
+  end
+  it "changes the orientation of robot" do
+    robot1.set_position(2,5)
+    robot1.set_orientation("NORTH")    
+    expect(robot1.change_orientation("LEFT")).to eq("WEST")
   end
   it "sets the position of robot" do
     expect(robot1.set_position(2, 5)).to eq ([2, 5])
   end
   # it "moves the robot" do
-  #   robot1.change_orientation("NORTH")
+  #   robot1.set_position(2,5)
+  #   robot1.set_orientation("NORTH")
+  #   expect(robot1.move).to eq([3, 5])
 
   # end
 end
